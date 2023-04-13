a8211115a7510d78cd354968d89aa1075175f863
adikul25 <67463318+adikul25@users.noreply.github.com>
2023-04-14 00:10:46 +0530

    Update QuestionGen.py

--- QuestionGen.py
+++ QuestionGen.py
@@ -21,20 +21,20 @@ from utils import generate_questions
 
 st.title("Question Generation App")
 
-# Get the folder path and number of questions to generate from the user
-uploaded_folder = st.file_uploader("Upload a folder", type="zip")
-if uploaded_folder is not None:
-    folder_path = os.path.join("data", uploaded_folder.name.split(".")[0])
-    with open(folder_path, "wb") as f:
-        f.write(uploaded_folder.getbuffer())
+# Get the zip file and number of questions to generate from the user
+zip_file = st.file_uploader("Upload a zip file containing text files:", type="zip")
+num_questions = st.number_input("Enter the number of questions to generate:", value=3)
 
-    num_questions = st.number_input("Enter the number of questions to generate:", value=3)
-
-    # Generate the questions and download the output file
-    if st.button("Generate Questions"):
+# Generate the questions and download the output file
+if st.button("Generate Questions"):
+    if zip_file is not None:
         with st.spinner("Generating Questions..."):
-            df = generate_questions(folder_path, num_questions)
+            df = generate_questions(zip_file, num_questions)
             st.success("Questions Generated!")
             st.dataframe(df)
             output_text = df.to_csv(sep="\t", index=False)
+    else:
+        st.warning("Please upload a zip file containing text files.")
+        #clipboard.copy(output_text)
+        #st.info("The generated questions have been copied to the clipboard as tab-separated text.")
 
