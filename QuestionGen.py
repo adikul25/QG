63de70138cd72b1343f6d3e39751eff00df6743d
adikul25 <67463318+adikul25@users.noreply.github.com>
2023-04-15 18:28:39 +0530

    Update QuestionGen.py

--- QuestionGen.py
+++ QuestionGen.py
@@ -1,40 +1,32 @@
-# import streamlit as st
-# from utils import generate_questions
-
-# st.title("Question Generation App")
-
-# # Get the folder path and number of questions to generate from the user
-# folder_path = st.text_input("Enter the folder path:")
-# num_questions = st.number_input("Enter the number of questions to generate:", value=3)
-
-# # Generate the questions and download the output file
-# if st.button("Generate Questions"):
-#     with st.spinner("Generating Questions..."):
-#         df = generate_questions(folder_path, num_questions)
-#         st.success("Questions Generated!")
-#         st.dataframe(df)
-#         output_text = df.to_csv(sep="\t", index=False)
-
 import os
 import streamlit as st
-from utils import generate_questions
+from utils import generate_questions, generate_file_questions1
 
 st.title("Question Generation App")
 
-# Get the zip file and number of questions to generate from the user
-zip_file = st.file_uploader("Upload a zip file containing text files:", type="zip")
+
+
+# Get the file or folder path and number of questions to generate from the user
+path_choice = st.selectbox("Select path type:", options=["File", "Folder"])
+if path_choice == "File":
+    input_path = st.file_uploader("Upload a text file:", type=["txt"])
+else:
+    input_path = st.file_uploader("Upload a text file:", type=["zip"])
+
 num_questions = st.number_input("Enter the number of questions to generate:", value=3)
 
 # Generate the questions and download the output file
 if st.button("Generate Questions"):
-    if zip_file is not None:
+    if input_path:
         with st.spinner("Generating Questions..."):
-            df = generate_questions(zip_file, num_questions)
+            if path_choice == "File":
+                df = generate_file_questions(input_path, num_questions)
+            else:
+                df = generate_questions(input_path, num_questions)
             st.success("Questions Generated!")
             st.dataframe(df)
             output_text = df.to_csv(sep="\t", index=False)
     else:
-        st.warning("Please upload a zip file containing text files.")
-        #clipboard.copy(output_text)
-        #st.info("The generated questions have been copied to the clipboard as tab-separated text.")
+        st.warning("Please input a file or folder path.")
+
 
